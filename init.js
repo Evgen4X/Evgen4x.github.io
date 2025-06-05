a8f06fab6a38d7689efec59e89fea7649ea7fd8d
Evgen4X <y.maskaiev.5555@gmail.com>
2025-06-05 15:58:44 +0200

    HUH

--- init.js
+++ init.js
@@ -1,5 +1,13 @@
 const header = document.querySelector("header");
 const footer = document.querySelector("footer");
 
-// header.innerHTML = "<h1>HEADER</h1>";
-// footer.innerHTML = "<h1>FOOTER</h1>";
+const url = new URL(document.baseURI).origin;
+
+header.innerHTML = `
+<h1>Lokalsi Tarnów</h1>
+<button id="search"><img class="icon" src="${url}/searchIcon.png"></button>
+`;
+footer.innerHTML = `
+<div class="footer-option">a</div>
+<div class="footer-option">d</div>
+<div class="footer-option">c</div>`;
